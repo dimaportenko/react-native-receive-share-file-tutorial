48bcbd531565eb9b6444957c20e41138350f74dc
Dima Portenko <troublesohard@gmail.com>
2022-07-10 15:50:31 +0300

    andriod implementation

--- android/app/src/main/java/com/rnrecievesharetutorial/MainActivity.java
+++ android/app/src/main/java/com/rnrecievesharetutorial/MainActivity.java
@@ -1,5 +1,7 @@
 package com.rnrecievesharetutorial;
 
+import android.content.Intent;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -44,5 +46,11 @@ public class MainActivity extends ReactActivity {
       // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     }
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    setIntent(intent);
   }
 }
---
+++ src/useGetShare.tsx
@@ -0,0 +1,35 @@
+import ReceiveSharingIntent from 'react-native-receive-sharing-intent';
+import {useEffect, useState} from 'react';
+
+export type ShareFile = {
+  filePath?: string;
+  text?: string;
+  weblink?: string;
+  mimeType?: string;
+  contentUri?: string;
+  fileName?: string;
+  extension?: string;
+};
+
+export const useGetShare = () => {
+  const [share, setShare] = useState<ShareFile[] | undefined>(undefined);
+
+  useEffect(() => {
+    // To get All Recived Urls
+    ReceiveSharingIntent.getReceivedFiles(
+      (files: ShareFile[]) => {
+        // files returns as JSON Array example
+        //[{ filePath: null, text: null, weblink: null, mimeType: null, contentUri: null, fileName: null, extension: null }]
+        console.log('Received Files', files);
+        setShare(files);
+      },
+      // @ts-ignore
+      error => {
+        console.log(error);
+      },
+      'ShareMedia', // share url protocol (must be unique to your app, suggest using your apple bundle id)
+    );
+  }, []);
+
+  return share;
+};
